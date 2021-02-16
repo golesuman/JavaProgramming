53254bf1d521946e5536afc4c67237ef2e8a4da1
Suman Gole <60792767+golesuman@users.noreply.github.com>
2021-02-16 15:18:45 +0545

    Add files via upload

---
+++ Basic/Main.java
@@ -0,0 +1,17 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+       Scanner scn=new Scanner(System.in);
+
+       String str=scn.nextLine();
+        System.out.println(str);
+        int a=5;
+        int b=6;
+        System.out.println(Math.max(a,b));
+
+    }
+}
